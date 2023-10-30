736cfc648a0e26a021fadb4d7e6bf770bd4aeb49
Piotr <piotrkun@gmail.com>
2023-10-30 13:18:44 +0100

    Refactor WriteOutputDataIntegrationTest for Streamlined Functional Flow
    - Transitioned file processing to use Java Streams for a more declarative approach.
    - Ensured early failure for missing files by using findFirst with an orElseThrow construct.
    - Replaced while-loop with Stream.generate for reading Parquet records, enhancing readability.

--- src/test/java/com/walkme/usecases/WriteOutputDataIntegrationTest.java
+++ src/test/java/com/walkme/usecases/WriteOutputDataIntegrationTest.java
@@ -1,12 +1,15 @@
 package com.walkme.usecases;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.walkme.entities.ActivityAccumulator;
 import com.walkme.generated.DailyActivityAggregate;
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.file.Files;
-import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.runtime.testutils.MiniClusterResourceConfiguration;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
@@ -14,7 +17,9 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.test.junit5.MiniClusterExtension;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.parquet.avro.AvroParquetReader;
+import org.apache.parquet.hadoop.ParquetReader;
 import org.apache.parquet.hadoop.util.HadoopInputFile;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -38,7 +43,8 @@ class WriteOutputDataIntegrationTest {
   private static DataStreamSource<ActivityAccumulator> testData(StreamExecutionEnvironment env) {
     return env.fromElements(
         new ActivityAccumulator("2023-10-12", "user1", "testEnv", "activityType1", 100L),
-        new ActivityAccumulator("2023-10-12", "user2", "testEnv2", "activityType2", 200L)
+        new ActivityAccumulator("2023-10-12", "user2", "testEnv2", "activityType2", 200L),
+        new ActivityAccumulator("2023-10-12", "user3", "testEnv3", "activityType3", 300L)
     );
   }
 
@@ -54,29 +60,44 @@ class WriteOutputDataIntegrationTest {
     sut.execute(testData(env), new Path(tempDir.toUri()));
     env.execute("should_correctly_write_output_files");
 
-    try (var stream = Files.walk(tempDir.resolve("year=2023/month=10/day=12/"))) {
-      var files = stream
-          .filter(Files::isRegularFile)
-          .toList();
-      assertThat(files).hasSize(1);
+    var dailyActivityAggregates = readOutputFileContent();
+    assertThat(dailyActivityAggregates).hasSize(3);
+    assertThat(dailyActivityAggregates).contains(
+        new DailyActivityAggregate("2023-10-12", "user1", "testEnv", "activityType1", 100L));
+    assertThat(dailyActivityAggregates).contains(
+        new DailyActivityAggregate("2023-10-12", "user2", "testEnv2", "activityType2", 200L));
+    assertThat(dailyActivityAggregates).contains(
+        new DailyActivityAggregate("2023-10-12", "user3", "testEnv3", "activityType3", 300L));
+  }
 
-      var hadoopPath = new org.apache.hadoop.fs.Path(files.get(0).toUri());
-      var inputFile = HadoopInputFile.fromPath(hadoopPath, new Configuration());
-
-      try (var reader = AvroParquetReader
-          .<DailyActivityAggregate>builder(inputFile)
-          .build()) {
-
-        var records = new ArrayList<>();
-        DailyActivityAggregate activityAggregate;
-        while ((activityAggregate = reader.read()) != null) {
-          records.add(activityAggregate);
-        }
-
-        assertThat(records).hasSize(2);
-        assertThat(records).contains(new DailyActivityAggregate("2023-10-12", "user1", "testEnv", "activityType1", 100L));
-        assertThat(records).contains(new DailyActivityAggregate("2023-10-12", "user2", "testEnv2", "activityType2", 200L));
-      }
+  private List<DailyActivityAggregate> readOutputFileContent() throws IOException {
+    try (var fileStream = Files.walk(tempDir.resolve("year=2023/month=10/day=12"))) {
+      return fileStream.filter(Files::isRegularFile)
+          .findFirst()
+          .map(this::readParquetFile)
+          .orElseThrow(() -> new IOException("No output file found"));
     }
+  }
+
+  private List<DailyActivityAggregate> readParquetFile(java.nio.file.Path filePath) throws UncheckedIOException {
+    try (var reader = AvroParquetReader.<DailyActivityAggregate>builder(
+        HadoopInputFile.fromPath(new org.apache.hadoop.fs.Path(filePath.toUri()), new Configuration())).build()) {
+      return readerToStream(reader)
+          .takeWhile(Objects::nonNull)
+          .toList();
+    } catch (IOException e) {
+      throw new UncheckedIOException("Failed to read parquet file", e);
+    }
+  }
+
+  @NotNull
+  private Stream<DailyActivityAggregate> readerToStream(ParquetReader<DailyActivityAggregate> reader) {
+    return Stream.generate(() -> {
+      try {
+        return reader.read();
+      } catch (IOException e) {
+        throw new UncheckedIOException(e);
+      }
+    });
   }
 }
